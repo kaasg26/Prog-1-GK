bfafb198aa610eaa0d9b1f2d32ea63ad8e9cdcd3
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-04-08 10:06:22 -0500

    4/8

---
+++ Fruitloops/Fruitloops/Program.py
@@ -0,0 +1,18 @@
+﻿colors = ["red","green","blue"]
+print(colors)
+print(colors[1]) #green
+
+fruits = ["apple","banana","orange","kiwi","mango"]
+#for-each loop
+for item in fruits:
+	print(item)
+print("")
+
+#for loop
+for index in range(len(fruits)):
+	print(fruits[index])
+print("")
+
+lastfruit = fruits[len(fruits)-1]
+print(lastfruit)
+input()
