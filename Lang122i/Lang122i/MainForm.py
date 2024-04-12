251d32bcd57c1d2a8fd34cb13953502940d1e879
kaasg26 <162478377+kaasg26@users.noreply.github.com>
2024-04-12 10:25:59 -0500

    4/12

--- Lang122i/Lang122i/MainForm.py
+++ Lang122i/Lang122i/MainForm.py
@@ -1,5 +1,4 @@
 ﻿import math
-import cmath
 import System.Drawing
 import System.Windows.Forms
 
@@ -20,15 +19,15 @@ class MainForm(Form):
 		# listBox1
 		# 
 		self._listBox1.FormattingEnabled = True
-		self._listBox1.Location = System.Drawing.Point(12, 12)
+		self._listBox1.Location = System.Drawing.Point(13, 13)
 		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(709, 329)
+		self._listBox1.Size = System.Drawing.Size(265, 355)
 		self._listBox1.TabIndex = 0
 		self._listBox1.SelectedIndexChanged += self.ListBox1SelectedIndexChanged
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(12, 364)
+		self._button1.Location = System.Drawing.Point(315, 37)
 		self._button1.Name = "button1"
 		self._button1.Size = System.Drawing.Size(75, 23)
 		self._button1.TabIndex = 1
@@ -38,7 +37,7 @@ class MainForm(Form):
 		# 
 		# button2
 		# 
-		self._button2.Location = System.Drawing.Point(93, 364)
+		self._button2.Location = System.Drawing.Point(315, 66)
 		self._button2.Name = "button2"
 		self._button2.Size = System.Drawing.Size(75, 23)
 		self._button2.TabIndex = 2
@@ -48,7 +47,7 @@ class MainForm(Form):
 		# 
 		# button3
 		# 
-		self._button3.Location = System.Drawing.Point(174, 364)
+		self._button3.Location = System.Drawing.Point(315, 96)
 		self._button3.Name = "button3"
 		self._button3.Size = System.Drawing.Size(75, 23)
 		self._button3.TabIndex = 3
@@ -58,29 +57,24 @@ class MainForm(Form):
 		# 
 		# MainForm
 		# 
-		self.ClientSize = System.Drawing.Size(733, 399)
+		self.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+		self.ClientSize = System.Drawing.Size(444, 378)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
 		self.Controls.Add(self._listBox1)
 		self.Name = "MainForm"
-		self.Text = "Lang122h"
+		self.Text = "Lang122i"
 		self.ResumeLayout(False)
 
 
-	def ListBox1SelectedIndexChanged(self, sender, e):
-		pass
-
 	def Button1Click(self, sender, e):
-		heading = "Number\t\tSquare\t\tSquare Root\t\tCube\t\t4th Root"
+		heading = "Number\t\tCube\t\tCube Root"
 		self._listBox1.Items.Add(heading)
-		for num in range(1, 20+1):
-			nsqrd = num**2
-			nsqrt = math.sqrt(num)
-			csqrt = num**3
-			Fsqrt = math.sqrt(math.sqrt(num))
-			
-			msg = str(num) + "\t\t" + str(nsqrd) + "\t\t" + str(round(nsqrt, 4)) + "\t\t\t" + str(csqrt) + "\t\t" + str(round(Fsqrt, 4))
+		for num in range(-25, 25+1):
+			nsqrd = num**3
+			csqrt = math.sqrt(num)
+			msg = str(num) + "\t\t" + str(nsqrd) + "\t\t" + str(round(csqrt, 4))
 			self._listBox1.Items.Add(msg)
 
 	def Button2Click(self, sender, e):
@@ -88,3 +82,6 @@ class MainForm(Form):
 
 	def Button3Click(self, sender, e):
 		Application.Exit()
+
+	def ListBox1SelectedIndexChanged(self, sender, e):
+		pass
